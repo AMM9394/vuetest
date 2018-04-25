e81b947665f8cead0c6358225ce7e71148d6ba66
GuoMiaomiao <guomiaomiao@fiberhome.com>
2018-04-25 15:31:44 +0800

    菜单组件封装及显示

--- src/mock/data.js
+++ src/mock/data.js
@@ -10,45 +10,45 @@ export const submenu = [
     submenu: [
       {
         title: '个人信息管理',
-        url: '#/personal',
-      },{
+        url: '#/personal'
+      }, {
         title: '工资查询',
-        url: '#/salary',
-      },{
+        url: '#/salary'
+      }, {
         title: '医疗记录',
-        url: '#/test',
-      },{
+        url: '#/test'
+      }, {
         title: '供给关系',
-        url: '#/test',
-      },{
+        url: '#/test'
+      }, {
         title: '被服领取',
-        url: '#/uniform',
-      },{
-        title:'住房查询',
-        url:'#/house',
-      },{
-        title:'保障标准',
-        url:'#/test',
-      },{
-        title:'保障卡管理',
-        url:'#/test',
-        submenu:[
+        url: '#/uniform'
+      }, {
+        title: '住房查询',
+        url: '#/house'
+      }, {
+        title: '保障标准',
+        url: '#/test'
+      }, {
+        title: '保障卡管理',
+        url: '#/test',
+        submenu: [
           {
-            title:'当前卡信息',
-            url:'#/bzkbasic',
-          },{
-            title:'补换卡申请',
-            url:'#/test',
-          },{
-            title:'历史记录',
-            url:'#/test',
-          },
-        ],
-      },
-    ],
-  },{
-    title:'用户管理',
-    url:'#/test',
+            title: '当前卡信息',
+            url: '#/bzkbasic'
+          }, {
+            title: '补换卡申请',
+            url: '#/test'
+          }, {
+            title: '历史记录',
+            url: '#/test'
+          }
+        ]
+      }
+    ]
+  }, {
+    title: '用户管理',
+    url: '#/test'
     // icon:userLogo,
-  },
-];
+  }
+]
